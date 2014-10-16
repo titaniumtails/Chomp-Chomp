561eacb49c24bb90e52ea116354b9ccaedb824e4
talia scotchbrook <taliaisontheweb@gmail.com>
2014-10-16 13:24:14 +0800

    Adds the Welcome Controller from Old Chomps which directs to site and under contstruction

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,13 @@
 class WelcomeController < ApplicationController
+
   def index
+    
+    if params[:admin] == "true"
+      render 'index'
+    else
+      render 'underconstruction', layout: false
+    end
+
   end
+
 end
